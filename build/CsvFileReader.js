8471d8d8773fb1b67bd1cfaaadaae8141303ce2c
Ismayil Ismayilov <Psixo2015@mail.ru>
2024-03-26 12:51:01 +0400

    add mapRow

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -16,15 +16,16 @@ class CsvFileReader {
         })
             .split('\n')
             .map((row) => row.split(','))
-            .map((row) => {
-            return [
-                (0, utils_1.dateToString)(row[0]),
-                row[1], row[2],
-                +row[3], +row[4],
-                row[5],
-                row[6]
-            ];
-        });
+            .map(this.mapRow);
+    }
+    mapRow(row) {
+        return [
+            (0, utils_1.dateToString)(row[0]),
+            row[1], row[2],
+            +row[3], +row[4],
+            row[5],
+            row[6]
+        ];
     }
 }
 exports.default = CsvFileReader;
